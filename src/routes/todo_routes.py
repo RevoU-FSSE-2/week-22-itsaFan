3c50c6e83f677530f5ec5c00cc72d4bc0ed997ba
itsaFan <steffansim@gmail.com>
2023-11-30 23:56:58 +0800

    add edit func

--- src/routes/todo_routes.py
+++ src/routes/todo_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from db_config import db  
-from services.todo_services import create_todo, get_todo_by_creator, get_all_todos, search_todos, get_todo_by_id, delete_todo
+from services.todo_services import create_todo, get_todo_by_creator, get_all_todos, search_todos, get_todo_by_id, delete_todo, update_todo
 from services.user_services import get_current_user, is_user_authorized
 
 
@@ -102,6 +102,37 @@ def delete_todo_route(todo_id):
             return jsonify({"message": "Todo not found"}), 404
 
         return jsonify({"message": "Todo deleted successfully"}), 200
+
+    except ValueError as ve:
+        return jsonify({"error": str(ve)}), 401
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@todo_bp.route('/edit/<string:todo_id>', methods=['PUT'])
+def edit_todo_route(todo_id):
+    try:
+        current_user = get_current_user()
+        if not current_user:
+            return jsonify({"message": "Unauthorized"}), 401
+
+        user_id = current_user.get('userId')
+        user_role = current_user.get('role')
+
+        todo = get_todo_by_id(todo_id)
+        if not todo:
+            return jsonify({"message": "Todo not found"}), 404
+
+        if str(todo['createdBy']) != user_id and user_role != "ROLE_ADMIN":
+            return jsonify({"message": "Only the creator or admin can edit it"}), 403
+
+        new_todo_data = request.json
+        success = update_todo(todo_id, new_todo_data)
+        if not success:
+            return jsonify({"message": "Todo update failed"}), 400
+
+        updated_todo = get_todo_by_id(todo_id)
+        return jsonify({"message": "Todo updated successfully", "todo": updated_todo}), 200
 
     except ValueError as ve:
         return jsonify({"error": str(ve)}), 401
--- src/services/todo_services.py
+++ src/services/todo_services.py
@@ -57,14 +57,22 @@ def get_all_todos():
 
 
 def get_todo_by_id(todo_id):
-    return db.todos.find_one({"_id": ObjectId(todo_id)})
+    todo = db.todos.find_one({"_id": ObjectId(todo_id)})
+    if todo:
+        todo['_id'] = str(todo['_id'])
+        todo['createdBy'] = str(todo['createdBy'])
+    return todo
+
 
 def delete_todo(todo_id):
     return db.todos.delete_one({"_id": ObjectId(todo_id)}).deleted_count
 
 def update_todo(todo_id, new_todo_data):
-    updated_data = {k: v for k, v in new_todo_data.items() if v is not None}  
-    return db.todos.update_one({"_id": ObjectId(todo_id)}, {"$set": updated_data}).modified_count
+    updates = {k: v for k, v in new_todo_data.items() if v is not None}
+    if 'deadline' in updates and updates['deadline']:
+        updates['deadline'] = datetime.datetime.strptime(updates['deadline'], '%Y-%m-%d')
+    result = db.todos.update_one({"_id": ObjectId(todo_id)}, {"$set": updates})
+    return result.modified_count > 0
 
 
 
